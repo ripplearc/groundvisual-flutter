124781127f21790d60c0948ad44888cbd82b3258
Suyang Dong <sdong13@ford.com>
2020-11-06 07:28:21 -0500

    - Add a button at the same row with the toggle button

--- lib/appBar/LandingPageHeader.dart
+++ lib/appBar/LandingPageHeader.dart
@@ -26,21 +26,49 @@ class LandingPageHeader extends StatelessWidget {
             child: SiteDropDownList(),
           ),
           Padding(padding: EdgeInsets.all(2.0)),
-          ToggleSwitch(
-            minWidth: 55.0,
-            minHeight: 20.0,
-            iconSize: 12,
-            fontSize: 12,
-            cornerRadius: 20.0,
-            activeBgColor: Theme.of(context).colorScheme.primary,
-            activeFgColor: Colors.white,
-            inactiveBgColor: Theme.of(context).colorScheme.onBackground,
-            inactiveFgColor: Colors.white,
-            labels: ['Day', 'Trend'],
-            onToggle: (index) {
-              print('switched to: $index');
-            },
-          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ToggleSwitch(
+                  minWidth: 55.0,
+                  minHeight: 20.0,
+                  iconSize: 12,
+                  fontSize: 12,
+                  cornerRadius: 20.0,
+                  activeBgColor: Theme.of(context).colorScheme.primary,
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Theme.of(context).colorScheme.onBackground,
+                  inactiveFgColor: Colors.white,
+                  labels: ['Day', 'Trend'],
+                  onToggle: (index) {
+                    print('switched to: $index');
+                  },
+                ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                  ),
+                ),
+                Container(
+                    height: 20,
+                    child: FlatButton.icon(
+                      height: 20,
+                      icon: Icon(
+                        Icons.add_alarm,
+                        size: 16,
+                      ),
+                      label: Text(
+                        'Today',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      textColor: Theme.of(context).colorScheme.primary,
+                      padding: EdgeInsets.all(0),
+                      color: null,
+                      onPressed: () {},
+                    ))
+              ]),
           Padding(padding: EdgeInsets.all(4.0)),
         ],
       ),
