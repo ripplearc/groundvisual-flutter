5a675a7d4f30ff1be22a48bdbd3786d90373a3bb
Suyang Dong <sdong13@ford.com>
2020-12-10 07:48:45 -0500

    - Set the light and dark mode of the Google Map. - Eagerly Intercept the gesture

--- lib/app/ground_visual_app.dart
+++ lib/app/ground_visual_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:groundvisual_flutter/app/root_home_page.dart';
 import 'package:groundvisual_flutter/components/theme/app_theme.dart';
+import 'package:groundvisual_flutter/landing/body/landing_page_body.dart';
 
 class GroundVisualApp extends StatelessWidget {
 
@@ -15,3 +16,13 @@ class GroundVisualApp extends StatelessWidget {
     );
   }
 }
+
+// class GroundVisualApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Google Maps Demo',
+//       home: MapSample(),
+//     );
+//   }
+// }
--- lib/landing/body/landing_page_body.dart
+++ lib/landing/body/landing_page_body.dart
@@ -1,13 +1,19 @@
+import 'dart:async';
+
 import 'package:fluro/fluro.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:groundvisual_flutter/landing/bloc/selected_site_bloc.dart';
 import 'package:groundvisual_flutter/landing/chart/date/working_time_daily_chart.dart';
 import 'package:groundvisual_flutter/landing/chart/date/working_time_daily_chart_shimmer.dart';
 import 'package:groundvisual_flutter/landing/chart/trend/working_time_trend_chart.dart';
 import 'package:groundvisual_flutter/landing/chart/trend/working_time_trend_chart_shimmer.dart';
 import 'package:groundvisual_flutter/landing/chart/trend/working_time_trend_chart_viewmodel.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class LandingHomePageBody extends StatelessWidget {
   @override
@@ -16,9 +22,11 @@ class LandingHomePageBody extends StatelessWidget {
           builder: (context, state) => SliverList(
                 delegate: SliverChildBuilderDelegate(
                   (BuildContext context, int index) {
-                    return _displayWorkingTimeChart(state);
+                    return index == 0
+                        ? _displayWorkingTimeChart(state)
+                        : AspectRatio(aspectRatio: 1.8, child: MapSample());
                   },
-                  childCount: 1,
+                  childCount: 2,
                 ),
               ));
 
@@ -35,9 +43,6 @@ class LandingHomePageBody extends StatelessWidget {
       return Container();
     }
   }
-
-
-
 
   void _tapDetail(BuildContext context, FluroRouter router, String key) {
     String message = "";
@@ -69,3 +74,84 @@ class ListElement extends StatelessWidget {
   Widget build(BuildContext context) => Text("List tile $index");
 }
 
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> with WidgetsBindingObserver {
+  Completer<GoogleMapController> _controller = Completer();
+  String _darkMapStyle;
+  String _lightMapStyle;
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _loadMapStyles();
+    _setMapStyle();
+  }
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
+          Factory<OneSequenceGestureRecognizer>(
+            () => EagerGestureRecognizer(),
+          ),
+        ].toSet(),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future _loadMapStyles() async {
+    _darkMapStyle = await rootBundle.loadString('assets/map_styles/dark.json');
+    _lightMapStyle =
+        await rootBundle.loadString('assets/map_styles/light.json');
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    _setMapStyle();
+  }
+
+  Future _setMapStyle() async {
+    final controller = await _controller.future;
+    final theme = WidgetsBinding.instance.window.platformBrightness;
+    if (theme == Brightness.dark)
+      controller.setMapStyle(_darkMapStyle);
+    else
+      controller.setMapStyle(_lightMapStyle);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+}
