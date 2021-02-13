ffb822ebc73efb045f3f040385dc94e5c8451ee8
Suyang Dong <sdong13@ford.com>
2021-02-13 06:12:41 -0500

    - Attempt to launch in web with Flutter Beta but only get black screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:groundvisual_flutter/di/di.dart';
 
@@ -8,10 +6,11 @@ import 'app/ground_visual_app.dart';
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureInjection();
-  return runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => GroundVisualApp(),
-    ),
-  );
+  return GroundVisualApp();
+  // return runApp(
+  //   DevicePreview(
+  //     enabled: !kReleaseMode,
+  //     builder: (context) => GroundVisualApp(),
+    // ),
+  // );
 }
