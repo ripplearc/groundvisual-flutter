4e10bf3def190f70b99973fb8414061989754327
Suyang Dong <sdong13@ford.com>
2021-03-16 06:49:39 -0400

    - Launch dialog for trend/daily selection in tablet or desktop, - Launch bottomSheet for trend/daily selection in tablet or desktop - Extract PeriodSheet widget

--- lib/landing/appbar/component/selection/trend_period_selection_button.dart
+++ lib/landing/appbar/component/selection/trend_period_selection_button.dart
@@ -2,93 +2,71 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:groundvisual_flutter/component/buttons/date_button.dart';
+import 'package:groundvisual_flutter/component/period_sheet.dart';
 import 'package:groundvisual_flutter/landing/appbar/bloc/selected_site_bloc.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+typedef _OnTapAction(BuildContext context, SelectedSiteAtTrend state);
 
 /// Select a period to display the information about the selected site. It selects
 /// the last 7 days by default, and it resets to last 7 days when toggle between sites,
 /// or between date and trend. Once a new period is selected, it updates the <SelectedSiteBloc>.
 class TrendPeriodSelectionButton extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
+  Widget build(BuildContext context) => ScreenTypeLayout.builder(
+        mobile: (_) =>
+            _buildTrendButton(_buildTrendWindowSelectionInBottomSheet),
+        tablet: (_) => _buildTrendButton(_buildTrendWindowSelectionInDialog),
+        desktop: (_) => _buildTrendButton(_buildTrendWindowSelectionInDialog),
+      );
+
+  _buildTrendButton(_OnTapAction onTapAction) =>
       BlocBuilder<SelectedSiteBloc, SelectedSiteState>(
           buildWhen: (previous, current) => current is SelectedSiteAtTrend,
           builder: (context, state) => state is SelectedSiteAtTrend
               ? DateButton(
                   dateText: state.period.value(),
                   action: () {
-                    _buildTrendWindowSelection(
-                      context,
-                      state.period,
-                      (TrendPeriod period) {
-                        if (period == state.period) return;
-                        BlocProvider.of<SelectedSiteBloc>(context)
-                            .add(TrendSelected(period, context));
-                      },
-                    );
+                    onTapAction(context, state);
                   })
               : Container());
 
-  void _buildTrendWindowSelection(
-    BuildContext context,
-    TrendPeriod currentPeriod,
-    Function(TrendPeriod period) action,
-  ) =>
+  void _buildTrendWindowSelectionInBottomSheet(
+          BuildContext context, SelectedSiteAtTrend state) =>
       showModalBottomSheet(
           context: context,
-          backgroundColor: Theme.of(context).colorScheme.onSurface,
+          backgroundColor: Theme.of(context).cardTheme.color,
           builder: (_) => Container(
-                height: 350,
-                color: Theme.of(context).colorScheme.background,
-                child: ListView(
-                  children: [_buildHeaderTile(context)] +
-                      TrendPeriod.values.map((item) {
-                        if (item == currentPeriod) {
-                          return _buildHighlightListTile(item, context);
-                        } else {
-                          return _buildNormalListTile(item, context, action);
-                        }
-                      }).toList(),
-                ),
-              ));
+              height: 350,
+              child: PeriodSheet(
+                initialPeriod: state.period,
+                onSelectedTrendAction: (TrendPeriod period) {
+                  _onTrendSelected(period, state, context);
+                },
+              )));
 
-  ListTile _buildHeaderTile(BuildContext context) => ListTile(
-        title: Text("Trend Period",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.headline5),
-        contentPadding: EdgeInsets.all(20),
-      );
+  void _buildTrendWindowSelectionInDialog(
+          BuildContext scaffoldContext, SelectedSiteAtTrend state) =>
+      showDialog(
+          context: scaffoldContext,
+          builder: (_) => SimpleDialog(
+                  backgroundColor: Theme.of(scaffoldContext).cardTheme.color,
+                  children: [
+                    Container(
+                        width: 300,
+                        height: 330,
+                        child: PeriodSheet(
+                          initialPeriod: state.period,
+                          onSelectedTrendAction: (TrendPeriod period) {
+                            _onTrendSelected(period, state, scaffoldContext);
+                          },
+                        ))
+                  ]));
 
-  ListTile _buildHighlightListTile(
-    TrendPeriod item,
-    BuildContext context,
-  ) =>
-      ListTile(
-        title: Text(
-          item.value(),
-          style: Theme.of(context)
-              .textTheme
-              .subtitle1
-              .apply(color: Theme.of(context).colorScheme.primary),
-        ),
-        trailing: Icon(
-          Icons.check,
-          color: Theme.of(context).colorScheme.primary,
-        ),
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-      );
-
-  ListTile _buildNormalListTile(
-    TrendPeriod item,
-    BuildContext context,
-    Function(TrendPeriod period) action,
-  ) =>
-      ListTile(
-        title: Text(item.value(), style: Theme.of(context).textTheme.subtitle1),
-        onTap: () {
-          action(item);
-          Navigator.of(context).pop();
-        },
-      );
+  void _onTrendSelected(
+      TrendPeriod period, SelectedSiteAtTrend state, BuildContext context) {
+    if (period == state.period) return;
+    BlocProvider.of<SelectedSiteBloc>(context)
+        .add(TrendSelected(period, context));
+  }
 }
