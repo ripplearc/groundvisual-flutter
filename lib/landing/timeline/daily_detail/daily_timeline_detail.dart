a8374843bf90cc338029d241c32821241cd12c95
Suyang Dong <sdong13@ford.com>
2021-04-18 05:41:56 -0400

    - Create WorkZoneMap as a generic widget - Explore an approach to have rounded border, but the downside is the map is not interactable.

--- lib/landing/timeline/daily_detail/daily_timeline_detail.dart
+++ lib/landing/timeline/daily_detail/daily_timeline_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:groundvisual_flutter/component/map/workzone_map.dart';
 import 'package:groundvisual_flutter/extensions/collection.dart';
 import 'package:groundvisual_flutter/landing/timeline/daily_detail_photo/mobile/daily_detail_photo_mobile_view.dart';
 import 'package:groundvisual_flutter/landing/timeline/daily_detail_photo/web/daily_detail_photo_web_view.dart';
@@ -48,59 +49,126 @@ class _DailyTimelineDetailState extends State<DailyTimelineDetail> {
     _screenHeight = MediaQuery.of(context).size.height;
   }
 
+  static const double kCoverHeightProportion = 0.15;
+  static const double kBigBoxPadding = 8;
+  static const double kBottomBigBoxPadding = 60;
+
+  //
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: CustomScrollView(
+  //       slivers: <Widget>[
+  //         SliverAppBar(
+  //           expandedHeight: _screenHeight * 0.618,
+  //           flexibleSpace: FlexibleSpaceBar(
+  //             background: _buildMapAppBarContent(context),
+  //           ),
+  //         ),
+  //         SliverList(
+  //           delegate: SliverChildBuilderDelegate((_, __) {
+  //             return Container(
+  //               decoration: BoxDecoration(
+  //                   color: Colors.orange,
+  //                   borderRadius: BorderRadius.only(
+  //                       topLeft: Radius.circular(10),
+  //                       topRight: Radius.circular(10))),
+  //               child: Column(
+  //                 children: List.generate(
+  //                     10,
+  //                     (index) => Container(
+  //                         child: ListTile(title: Text("$index nothing")))),
+  //               ),
+  //             );
+  //           }, childCount: 1),
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
+    var topPadding = kCoverHeightProportion *
+        kCoverHeightProportion *
+        MediaQuery.of(context).size.height /
+        (kCoverHeightProportion *
+            (MediaQuery.of(context).size.height -
+                kBigBoxPadding -
+                kBottomBigBoxPadding));
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: _screenHeight * 0.618,
-            flexibleSpace: FlexibleSpaceBar(
-              background: _buildImageAppBarContent(context),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  width: _screenWidth,
-                  height: 50,
-                  child: Text("$index"),
-                );
-              },
-              childCount: 20,
-            ),
-          )
-        ],
-      ),
-      // ListView(
-      //   children: <Widget>[
-      //     Container(
-      //         width: _screenWidth - 64.0,
-      //         child: Hero(
-      //             tag: 'text' + widget.heroType.title,
-      //             child: Material(
-      //                 color: Colors.transparent,
-      //                 child: Text(
-      //                   '${widget.heroType.title}',
-      //                   style: TextStyle(
-      //                       fontSize: 24.0,
-      //                       fontWeight: FontWeight.bold,
-      //                       color: widget.heroType.materialColor),
-      //                 )))),
-      //     Container(
-      //         width: _screenWidth - 64.0,
-      //         child: Hero(
-      //             tag: 'subtitle' + widget.heroType.title,
-      //             child: Material(
-      //                 color: Colors.transparent,
-      //                 child: Text(
-      //                   widget.heroType.subTitle,
-      //                 )))),
-      //   ],
+        body: Stack(children: [
+      _buildMapAppBarContent(context),
+      // SliverAppBar(
+      //   expandedHeight: _screenHeight * 0.618,
+      //   flexibleSpace: FlexibleSpaceBar(
+      //     background: _buildMapAppBarContent(context),
+      //   ),
       // ),
-    );
+      Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              margin: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 10),
+
+              //width: MediaQuery.of(context).size.width * 0.9,
+              //margin: EdgeInsets.symmetric(horizontal: kBigBoxPadding),
+              decoration: BoxDecoration(
+                //color: Colors.pink,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+              ),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+                  child: CustomScrollView(
+                    anchor: 0,
+                    slivers: <Widget>[
+                      _buildContentHeader(topPadding),
+                      _buildContentBody()
+                    ],
+                  ))))
+    ]));
   }
+
+  SliverList _buildContentBody() {
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+      (BuildContext context, int index) {
+        return Container(
+          width: _screenWidth,
+          height: 50,
+          color: Colors.red,
+          child: Text("$index"),
+        );
+      },
+      childCount: 20,
+    ));
+  }
+
+  SliverPadding _buildContentHeader(double topPadding) => SliverPadding(
+      padding: EdgeInsets.only(
+        top: 200,
+      ),
+      sliver: SliverPersistentHeader(
+          pinned: true,
+          floating: false,
+          delegate: _SliverPersistentHeaderDelegate(Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Center(
+                  child: Text(
+                'La casa de don Juan',
+                style: TextStyle(
+                    color: Colors.orange,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w800),
+              ))))));
+
+  Widget _buildMapAppBarContent(BuildContext context) =>
+      Container(width: _screenWidth, height: 300, child: WorkZoneMap());
 
   Hero _buildImageAppBarContent(BuildContext context) => Hero(
         tag: 'image' +
@@ -183,4 +251,25 @@ class _DailyTimelineDetailState extends State<DailyTimelineDetail> {
         imageName,
         fit: BoxFit.cover,
       );
+}
+
+class _SliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+  _SliverPersistentHeaderDelegate(this.child);
+
+  final Widget child;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => 100;
+
+  @override
+  double get minExtent => 100;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => false;
 }
