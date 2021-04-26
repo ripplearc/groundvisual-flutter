d35b182ea89360b25d97cc84c54e07e801d675ff
Suyang Dong <sdong13@ford.com>
2021-04-26 09:10:52 -0400

    - Add persistent bottom navigation

--- lib/app/tablet/root_home_tablet_page.dart
+++ lib/app/tablet/root_home_tablet_page.dart
@@ -1,96 +1,51 @@
-import 'package:dart_date/dart_date.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:groundvisual_flutter/component/calendar_sheet.dart';
 import 'package:groundvisual_flutter/di/di.dart';
+import 'package:groundvisual_flutter/fleet/fleet_page_body.dart';
 import 'package:groundvisual_flutter/landing/appbar/bloc/selected_site_bloc.dart';
-import 'package:groundvisual_flutter/landing/appbar/tablet/landing_page_tablet_header.dart';
 import 'package:groundvisual_flutter/landing/landing_home_page.dart';
 import 'package:groundvisual_flutter/router/bottom_navigation.dart';
 import 'package:groundvisual_flutter/router/placeholder_navigation_page.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 /// Tablet layout of the root home page.
+
 class RootHomeTabletPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _RootHomeTabletPageState();
 }
 
-class _RootHomeTabletPageState extends State<RootHomeTabletPage> {
-  int _currentIndex = 0;
+class _RootHomeTabletPageState extends State<RootHomeTabletPage>
+    with BottomNavigation {
+  late PersistentTabController _controller;
 
-  final List<Function> _tabPages = [
-    () => LandingHomePage(),
-    () => DocumentHomePage(title: "Fleet"),
-    () => PlaceholderWidget(
-          pageTitle: "Fleet Page Under Construction",
+  @override
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+  }
+
+  List<Widget> get _screens => [
+        LandingHomePage(),
+        FleetHomePage(title: "Fleet"),
+        PlaceholderWidget(
+          pageTitle: "Document Page Under Construction",
           tab: SelectedTab.fleet,
         ),
-    () => PlaceholderWidget(
+        PlaceholderWidget(
           pageTitle: "Account Page Under Construction",
           tab: SelectedTab.account,
         )
-  ];
-
-  final List<Function> _tabHeaders = [
-    (BuildContext context) => buildLandingHomePageTabletHeader(context),
-    (BuildContext context) => AppBar(title: Text("fleet")),
-    (BuildContext context) => AppBar(title: Text("doc")),
-    (BuildContext context) => AppBar(title: Text("account")),
-  ];
+      ];
 
   @override
   Widget build(BuildContext context) => BlocProvider<SelectedSiteBloc>(
       create: (_) => getIt<SelectedSiteBloc>()..add(SelectedSiteInit(context)),
       child: Scaffold(
-          appBar: _tabHeaders[_currentIndex](context),
-          body: _tabPages[_currentIndex](),
-          bottomNavigationBar: BottomNavigation(action: _setCurrentIndex)));
-
-  void _setCurrentIndex(int index) => setState(() {
-        _currentIndex = index;
-      });
-}
-
-class DocumentHomePage extends StatefulWidget {
-  DocumentHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _DocumentHomePageState createState() => _DocumentHomePageState();
-}
-
-class _DocumentHomePageState extends State<DocumentHomePage> {
-  @override
-  Widget build(BuildContext context) => Center(
-          child: ElevatedButton(
-        child: Text('Show Calendar Dialog'),
-        onPressed: _showMaterialDialog,
+        body: buildTabBar(
+            controller: _controller,
+            screens: _screens,
+            context: context),
       ));
-
-  _showMaterialDialog() {
-    showDialog(
-        context: context,
-        builder: (ctx) => SimpleDialog(
-              backgroundColor: Theme.of(context).cardTheme.color,
-              children: [
-                _calenderSelection(ctx, Date.startOfToday, (DateTime t) {})
-              ],
-            ));
-  }
-
-  Widget _calenderSelection(
-    BuildContext context,
-    DateTime initialSelectedDate,
-    Function(DateTime t) action,
-  ) =>
-      Container(
-        height: 600,
-        width: 500,
-        child: CalendarSheet(
-            confirmSelectedDateAction: action,
-            initialSelectedDate: initialSelectedDate),
-      );
 }
---
+++ lib/fleet/fleet_page_body.dart
@@ -0,0 +1,47 @@
+import 'package:dart_date/dart_date.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:groundvisual_flutter/component/calendar_sheet.dart';
+
+class FleetHomePage extends StatefulWidget {
+  FleetHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _FleetHomePageState createState() => _FleetHomePageState();
+}
+
+class _FleetHomePageState extends State<FleetHomePage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+          child: ElevatedButton(
+              child: Text('Show Calendar Dialog'),
+              onPressed: _showMaterialDialog)));
+
+  _showMaterialDialog() {
+    showDialog(
+        context: context,
+        builder: (ctx) => SimpleDialog(
+              backgroundColor: Theme.of(context).cardTheme.color,
+              children: [
+                _calenderSelection(ctx, Date.startOfToday, (DateTime t) {})
+              ],
+            ));
+  }
+
+  Widget _calenderSelection(
+    BuildContext context,
+    DateTime initialSelectedDate,
+    Function(DateTime t) action,
+  ) =>
+      Container(
+        height: 600,
+        width: 500,
+        child: CalendarSheet(
+            confirmSelectedDateAction: action,
+            initialSelectedDate: initialSelectedDate),
+      );
+}
