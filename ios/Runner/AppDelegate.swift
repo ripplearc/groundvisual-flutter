6c7c34997276e4bf892555f0c9b452e012c497aa
Suyang Dong <ripplearc@gmail.com>
2023-05-19 08:25:50 -0400

    - Update the Readme - Stop tracking android/keys.properties ios/Runner/Keys.swift web/.env

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,10 +9,10 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-// TODO: Create a new file named Keys.swift in the ios/Runner directory of your Flutter project.
-//  Add your own Google Maps API key to the file Keys.swift
+//  TODO: Add your own Google Maps API key to the file ios/Runner/Keys.swift
 //  static let googleMapsApiKey = "YOUR_API_KEY"
 //  Apply for API key: https://developers.google.com/maps/documentation/embed/get-api-key#create-api-keys
+//  Make sure to enable the iOS SDK API for you API key
     GMSServices.provideAPIKey(Keys.googleMapsApiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
